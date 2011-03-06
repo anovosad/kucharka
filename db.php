5527c03e038cbb35f1407d42493c517b2e975ee6
ondras <none@none>
2011-03-06 21:46:47 +0100

    amounts

--- db.php
+++ db.php
@@ -23,6 +23,7 @@
 		}
 
 		public function getIngredients() {
+			/* FIXME postprocess */
 			return $this->query("SELECT ".self::INGREDIENT.".*, 
 								".self::CATEGORY.".name AS category_name
 								FROM ".self::INGREDIENT." 
@@ -33,19 +34,45 @@
 		public function getUsers() {
 			return $this->query("SELECT id, name FROM ".self::USER." ORDER by id ASC");
 		}
+		
+		/***/
 
 		public function getRecipe($id) {
 			$data = $this->query("SELECT * FROM ".self::RECIPE." WHERE id = ?", $id);
-			if (count($data)) { 
-				$data[0]["text"] = array(""=>$data[0]["text"]);
-				return $data[0]; 
-			}
-			return null; 
+			if (!count($data)) { return null; }
+			
+			$data = $data[0];
+			$data["text"] = array(""=>$data["text"]);
+			$data["remark"] = array(""=>$data["remark"]);
+			
+			$data["ingredients"] = array("ingredient"=>$this->getAmounts($id));
+			return $data; 
 		}
-
 
 		public function getType($id) {
 			return $this->query("SELECT * FROM ".self::TYPE." WHERE id = ?", $id);
+		}
+
+		public function getIngredient($id) {
+			return $this->query("SELECT * FROM ".self::INGREDIENT." WHERE id = ?", $id);
+		}
+
+		public function getCategory($id) {
+			return $this->query("SELECT * FROM ".self::CATEGORY." WHERE id = ?", $id);
+		}
+
+		public function getUser($id) {
+			return $this->query("SELECT * FROM ".self::USER." WHERE id = ?", $id);
+		}
+		
+		public function getAmounts($id_recipe) {
+			$data = $this->query("SELECT ".self::INGREDIENT.".*, ".self::AMOUNT.".amount
+									FROM ".self::AMOUNT."
+									LEFT JOIN ".self::INGREDIENT." ON ".self::AMOUNT.".id_ingredient = ".self::INGREDIENT.".id
+									LEFT JOIN ".self::CATEGORY." ON ".self::INGREDIENT.".id_category = ".self::CATEGORY.".id
+									WHERE ".self::AMOUNT.".id_recipe = ?
+									ORDER BY ".self::CATEGORY.".`order`", $id_recipe);
+			return $data;
 		}
 
 	}
